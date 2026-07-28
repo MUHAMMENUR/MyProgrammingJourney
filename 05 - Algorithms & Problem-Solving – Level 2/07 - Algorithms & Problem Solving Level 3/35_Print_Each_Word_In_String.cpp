c3b18ad64e5e12527dffd87dfaedcf1b68160cc1
MUHAMMENUR <muhmmednursanda@gmail.com>
2026-07-28 18:08:53 +0300

    Update 36 problem

--- 05 - Algorithms & Problem-Solving – Level 2/07 - Algorithms & Problem Solving Level 3/35_Print_Each_Word_In_String.cpp
+++ 05 - Algorithms & Problem-Solving – Level 2/07 - Algorithms & Problem Solving Level 3/35_Print_Each_Word_In_String.cpp
@@ -1,80 +1,42 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
 #include <string>
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
-// Vector containing all standard vowel characters
-// Defined globally or within the function for comparison
-vector <char> Vowel = { 'a', 'e', 'i', 'o', 'u' };
-
-
-
-/**
- * Function: ReadString
- * --------------------
- * Prompts the user to input a full sentence.
- * @return The complete string input.
- */
-string ReadString() {
-	string Input;
+string Readstring() {
+	string input;
 	cout << "Enter a string: ";
-	getline(cin,Input);
-	return Input;
+	getline(cin, input);
+	return input;
 }
 
-/**
- * Function: PrintWord
- * -------------------
- * Parses an input string and prints each word on a new line.
- * Handles trailing words and prevents empty lines caused by consecutive spaces.
- *
- * @param InputString The full sentence to break down into words.
- */
-void PrintWord(string InputString) {
-	cout << "Words in string are:" << endl;
-	string word = "";
-	// Iterate through every character in the string
-	for (char c : InputString) {
-		if (c == ' ') {
-			// Print the accumulated word when a delimiter (space) is met
-			if (!word.empty()) {
-				cout << word << endl;
-				word.clear();// Reset accumulator for the next word
-			}
+
+void PrintEachWordInString(string str) {
+	string delimiter = " ";
+	short pos = 0;
+	string sWord;
+
+
+	while ((pos = str.find(delimiter)) != std::string::npos) {
+		sWord = str.substr(0, pos);
+		if (sWord != ""){
+			cout<<sWord<<endl ;
 		}
-		else {
-			word += c;// Build the word character by character
-		}
+
+		str.erase(0, pos + delimiter.length());
 	}
-	if (!word.empty()) {
-		cout << word << endl;
+	if (str != "")
+	{
+		cout << str << endl; 
 	}
 }
 
 
 
-
-
-
-
-int main() {
-
-	
-	// Step 1: Read full text line from user
-	string InputString = ReadString();
-
-
-
-	// Step 2: Extract and display each word independently
-	PrintWord(InputString);
-
-
-	system("pause>0");
-
-	return 0;
-
-
+int main()
+{
+    
+	string input = Readstring();
+	PrintEachWordInString(input);
 }
+
