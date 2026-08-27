8be8bcd38352d1563d3711ebaba2e2df1993b462
MUHAMMENUR <muhmmednursanda@gmail.com>
2026-08-27 16:48:00 +0300

    Solve Problem 20: Implement functions to increase date across multiple time units

---
+++ 08 - Algorithms & Problem Solving Level 4/19_DiffrenceInDays_NegativeDays.cpp
@@ -0,0 +1,222 @@
+#pragma warning(disable : 4996)
+#include <iostream>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+
+
+struct sDate {
+	short Day;
+	short Month;
+	short Year;
+};
+
+// Function to determine whether a given year is a leap year
+bool isLeapYear(short Year)
+{
+	// A year is a leap year if it is divisible by 4 AND not by 100,
+	// OR if it is divisible by 400
+	return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
+}
+
+short DayOfWeekOrder(short Year, short Month, short Day) {
+	int a, y, m, d;
+	a = (14 - Month) / 12;
+	y = Year - a;
+	m = Month + 12 * a - 2;
+	d = (Day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7;
+
+	return d;
+}
+
+string DayOfWeekName(int d) {
+	switch (d) {
+	case 0: return "Sunday";
+	case 1: return "Monday";
+	case 2: return "Tuesday";
+	case 3: return "Wednesday";
+	case 4: return "Thursday";
+	case 5: return "Friday";
+	case 6: return "Saturday";
+	default: return "Invalid day";
+	}
+
+
+}
+
+short DaysInMonth(short Year, short Month) {
+	switch (Month) {
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12:
+		return 31;
+	case 4:
+	case 6:
+	case 9:
+	case 11:
+		return 30;
+	case 2:
+		return isLeapYear(Year) ? 29 : 28;
+	default:
+		return -1;
+	}
+}
+
+
+
+
+
+
+string MonthShortName(short MonthNumber)
+{
+	string Months[12] = { "January", "February", "March", "April", "May", "June",
+						  "July", "August", "September", "October", "November", "December" };
+	return Months[MonthNumber - 1];
+}
+
+void SwapDates(sDate& Date1, sDate& Date2)
+{
+	sDate TempDate;
+
+	TempDate = Date1;
+	Date1 = Date2;
+	Date2 = TempDate;
+}
+
+int GetNumberOfDaysFromBeginningOfYear(short Year, short Month, short Day) {
+	int totalDays = 0;
+	for (short m = 1; m < Month; ++m) {
+		totalDays += DaysInMonth(Year, m);
+	}
+	totalDays += Day;
+	return totalDays;
+}
+
+short ReadDay(string message) {
+	short day;
+	do {
+		cout << message;
+		cin >> day;
+	} while (day < 1 || day > 31);
+	return day;
+}
+
+short ReadMonth(string message) {
+	short month;
+	do {
+		cout << message;
+		cin >> month;
+	} while (month < 1 || month > 12);
+	return month;
+}	
+
+short ReadYear(string message) {
+	short year;
+	do {
+		cout << message;
+		cin >> year;
+	} while (year < 1900 || year > 2100);
+	return year;
+}
+
+sDate ReadFullDate()
+{
+	sDate Date;
+	Date.Day = ReadDay("Please enter a Day? ");
+	Date.Month = ReadMonth("Please enter a Month? ");
+	Date.Year = ReadYear("Please enter a Year? ");
+	return Date;
+}
+
+sDate GetSystemDate() {
+	sDate systemDate;
+	time_t now = time(0);
+	tm* ltm = localtime(&now);
+	systemDate.Year = 1900 + ltm->tm_year;
+	systemDate.Month = 1 + ltm->tm_mon;
+	systemDate.Day = ltm->tm_mday;
+	return systemDate;
+}
+bool IslastDayInMonth(sDate date) {
+
+	return date.Day == DaysInMonth(date.Year, date.Month);
+
+}
+bool IslastMonth(sDate date) {
+	return date.Month == 12;
+}
+
+sDate IncreseDateByOneDay(sDate date) {
+	if (IslastDayInMonth(date)) {
+		if (IslastMonth(date)) {
+			date.Day = 1;
+			date.Month = 1;
+			date.Year++;
+
+		}
+		else {
+			date.Day = 1;
+			date.Month++;
+		}
+	}
+	else {
+		date.Day++;
+	}
+	return date;
+}
+
+
+bool IsDate1BeforeDate2(sDate date1, sDate date2) {
+	if (date1.Year != date2.Year)
+		return date1.Year < date2.Year;
+
+	if (date1.Month != date2.Month)
+		return date1.Month < date2.Month;
+
+	return date1.Day < date2.Day;
+}
+
+int GetDifferenceInDays(sDate Date1, sDate Date2, bool
+	IncludeEndDay = false)
+{
+	int Days = 0;
+	short SawpFlagValue = 1;
+	if (!IsDate1BeforeDate2(Date1, Date2))
+	{
+		
+		SwapDates(Date1, Date2);
+		SawpFlagValue = -1;
+	}
+	while (IsDate1BeforeDate2(Date1, Date2))
+	{
+		Days++;
+		Date1 = IncreseDateByOneDay(Date1);
+	}
+	return IncludeEndDay ? ++Days * SawpFlagValue : Days *
+		SawpFlagValue;
+}
+int main()
+{
+	
+	
+	sDate Date1 = ReadFullDate();
+	sDate Date2 = ReadFullDate();
+	cout << "\nDiffrence is: "
+		<< GetDifferenceInDays(Date1, Date2) << " Day(s).";
+	cout << "\nDiffrence (Including End Day) is: "
+		<< GetDifferenceInDays(Date1, Date2, true) << " Day(s).";
+
+
+
+
+
+
+	system("pause>0");
+	return 0;
+}
